703dbe4068c61468bf21113d3ae8fb0830c6ed78
Zeus Chiu <zeuscsc@gmail.com>
2024-01-12 16:19:46 +0800

    Fix prompt generation in DeepSeekLLM class

--- llama_index_extra_llm/deepseek.py
+++ llama_index_extra_llm/deepseek.py
@@ -34,7 +34,7 @@ class DeepSeekLLM(HuggingFaceLLM):
             if message["role"] == "user":
                 prompt += f"""User: {message["content"]}\nAssistant: """
             else:
-                prompt += f"""Assistant: {messages[1]['content']}"""
+                prompt += f"""Assistant: {message['content']}"""
         return prompt
 
     def __init__(
